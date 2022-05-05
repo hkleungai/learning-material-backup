775f567a1bfed6c3b6053c49bc9b4a7cc634dee4
Jimmy Leung <hkleungai@connect.ust.hk>
2022-05-06 01:47:53 +0800

    Updates

--- data.ts
+++ data.ts
@@ -7,6 +7,14 @@ const categorizedCourses = {
       link: './COMP2012',
     },
   ],
+  'Algorithm Study': [
+    {
+      code: 'COMP 2711H',
+      title: 'Honors Discrete Mathematical Tools for Computer Science',
+      semester: 'Fall 2021',
+      link: './COMP2711H',
+    }
+  ],
   'Computer Architecture / Operating System': [
     {
       code: 'COMP 2611',
