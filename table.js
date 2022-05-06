e5ddec13e879e97bbc0be33cac90da9bf2e392c8
Jimmy Leung <hkleungai@connect.ust.hk>
2022-05-07 01:34:54 +0800

    Updates

--- table.js
+++ table.js
@@ -1,7 +1,15 @@
-var buildCourseRow = function (_a) {
-    var code = _a.code, title = _a.title, semester = _a.semester, link = _a.link;
+var buildSemesterCell = function (code, semester) {
+    var link = "".concat(code, " ").concat(semester).replace(/\s/g, '-');
     return (
-    /* html */ "\n  <tr>\n    <td style=\"width: 20%\">".concat(code, "</td>\n    <td>").concat(title, "</td>\n    <td style=\"width: 20%\"><a href=\"").concat(link, "\">").concat(semester, "</a></td>\n  </tr>\n  "));
+    /* html */ "\n    <td style=\"width: 20%\">\n      <a href=\"".concat(link, "\">").concat(semester, "</a>\n    </td>\n    "));
+};
+var buildCourseRow = function (_a) {
+    var code = _a.code, title = _a.title, semesters = _a.semesters;
+    var _b = semesters.map(function (semester) { return buildSemesterCell(code, semester); }), firstCell = _b[0], extraCells = _b.slice(1);
+    var firstRow = (
+    /* html */ "\n    <tr>\n      <td style=\"width: 20%\" rowSpan=\"".concat(semesters.length, "\">").concat(code, "</td>\n      <td rowSpan=\"").concat(semesters.length, "\">").concat(title, "</td>\n      ").concat(firstCell, "\n    </tr>\n    "));
+    var extraRows = extraCells.map(function (semester) { /* html */ return "<tr>".concat(semester, "</tr>"); });
+    return "".concat(firstRow, "\n").concat(extraRows);
 };
 var buildCategoryTable = function (category, courses) { return (
 /* html */ "\n  <div class=\"main-content\">\n    <h3 class=\"category\">".concat(category, "</h3>\n      <table class=\"course-list\">\n        <tr>\n          <th>Course Code</th>\n          <th>Course Title</th>\n          <th>Semester</th>\n        </tr>\n        ").concat(courses.map(buildCourseRow).join('\n'), "\n      </table>\n    </h3>\n  </div>\n  ")); };
